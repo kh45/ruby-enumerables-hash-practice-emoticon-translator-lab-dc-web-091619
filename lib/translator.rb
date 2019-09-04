205d4e8e39e75a3d66082ca453b177aa880ea966
industrious-modal-4420 <khaledhassan45@gmail.com>
2019-09-04 05:01:02 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -11,7 +11,7 @@ end
 
 def get_japanese_emoticon(path, emoticon)
    emotions = load_library(path)
-   emotions["get_meaning"][emoticon]
+   emotions["get_emoticon"][emoticon]
 end
 
 def get_english_meaning
